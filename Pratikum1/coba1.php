d49be1b3dd4d59e7e6bfe2c17021c2d23bcf65c1
Tiaranty Rahmalia Virgiaone <114557122+Tiaranty093@users.noreply.github.com>
2023-03-02 08:13:41 +0700

    Add files via upload

---
+++ Pratikum1/coba1.php
@@ -0,0 +1,8 @@
+<?php
+
+define('PHI',3.14);
+define('DBNAME', 'inventori');
+define('DBSERVER', 'localhost');
+
+echo 'nama databasenya :'. DBNAME;
+echo '<br> lokasi databasenya :'.DBSERVER;
---
+++ Pratikum1/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$nama = 'tiaranty';
+$umur = 20;
+$berat = 45;
+
+echo 'nama saya : '. $nama;
+echo '<br>umur saya :'. $umur;
+echo '<br> berat saya :'. $berat;
